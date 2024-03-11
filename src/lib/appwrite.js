fcaacbf1a06bee1f94d5111b5b8bf38a1fcb48bc
navjotNSK <navjotsingh.uiet@gmail.com>
2024-03-11 23:17:50 +0530

    Changes for appwrite interface added.

--- src/lib/appwrite.js
+++ src/lib/appwrite.js
@@ -3,8 +3,8 @@ import { Client, Account} from 'appwrite';
 export const client = new Client();
 
 client
-    .setEndpoint('https://cloud.appwrite.io/v1')
-    .setProject('65d8d16e5a6ca6874469'); // Replace with your project ID
+    .setEndpoint(process.env.REACT_APP_APPWRITE_ENDPOINT)
+    .setProject(process.env.REACT_APP_APPWRITE_PROJECT_ID); // Replace with your project ID
 
 export const account = new Account(client);
 export { ID } from 'appwrite';
